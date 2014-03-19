d9e9ace34ed6322e2cfc9769a15e86c2b3a42dcb
Thibaud Av <thibaud.avenier@gmail.com>
2014-03-19 18:39:04 +0100

    test 2

---
+++ catalogue.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title>TW2 - Projet</title>
+	<link rel="stylesheet" type="text/css" href="media/css/style.css">
+
+
+
+</head>
+<body>
+
+<div id="contenaire">
+	
+	<header>
+		<div id="titre">
+			<h1>MyPlay</h1>
+			<h2>Créé vos playlists </h2>
+		</div>
+ 
+		<nav>
+	        <ul>
+	            <li><a href="index.php">Playlist</a></li>
+	            <li><a href="catalogue.php" class="select">Catalogue</a></li>
+	        </ul>
+	    </nav>
+	</header>
+	<section>
+		<div id="c1">
+			<h2>Ajouter des musique</h2>
+			<br>
+			
+		</div>
+		<div id="c2">
+			<h2>Mon Cata</h2>
+			<br>
+		</div>
+
+	</section>
+
+
+
+</div>
+
+</body>
+</html>
